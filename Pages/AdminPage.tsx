28d6edff1820ddeb82b2608d8e671b6fc8b267c1
chenzunh23 <145881193+chenzunh23@users.noreply.github.com>
2024-07-05 13:09:49 +0800

    Delete Pages directory

--- Pages/AdminPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useHistory } from 'react-router'
-import './app.css'
-export function Admin(){
-    const history=useHistory()
-    return(<div style={{color:'blue',fontFamily:'宋体',fontSize:'20px'}}>退出吧！
-    <button onClick={()=>history.push("/")} className={'button-primary'}>back</button></div>)
-}
