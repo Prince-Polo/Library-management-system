c137a3995ac017e8f9b3dd6c6e6536c7db8f05e2
chenzunh23 <145881193+chenzunh23@users.noreply.github.com>
2024-07-05 13:11:00 +0800

    Frontend: Register&Login. Jumping to other pages.

--- login-panel/src/renderer.tsx
+++ login-panel/src/renderer.tsx
@@ -3,8 +3,9 @@ import { render } from 'react-dom'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import { Main } from 'Pages/Main'
 import { AnotherPage } from 'Pages/AnotherPage'
-import { Library } from 'Pages/library'
-
+import { Library } from 'Pages/Library'
+import {StudentRegister,StudentLogin,AdminLogin,AdminRegister} from 'Pages/Register'
+import { Admin } from 'Pages/AdminPage'
 const Layout = () => {
     return (
         <HashRouter>
@@ -12,6 +13,11 @@ const Layout = () => {
                 <Route path="/" exact component={Main} />
                 <Route path="/another" exact component={AnotherPage} />
                 <Route path={"/library"} exact component={Library}/>
+                <Route path={"/StudentRegister"} exact component={StudentRegister}/>
+                <Route path={"/Admin"}exact component={Admin}/>
+                <Route path={"/StudentLogin"} exact component={StudentLogin}/>
+                <Route path={"/AdminRegister"} exact component={AdminRegister}/>
+                <Route path={"/AdminLogin"} exact component={AdminLogin}/>
             </Switch>
         </HashRouter>
     )
