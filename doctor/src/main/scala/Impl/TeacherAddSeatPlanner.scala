dab67b8f34846b481258404a8da09afc458a1e35
sunwc22 <113444136+sunwc22@users.noreply.github.com>
2024-07-09 16:28:39 +0800

    TeacherDeleteSeatPlanner and TeacherAddSeatPlanner
    update for using API SeatMessage

--- doctor/src/main/scala/Impl/TeacherAddSeatPlanner.scala
+++ doctor/src/main/scala/Impl/TeacherAddSeatPlanner.scala
@@ -6,12 +6,13 @@ import Common.API.{PlanContext, Planner}
 import Common.DBAPI.{writeDB}
 import Common.Object.SqlParameter
 import Common.ServiceUtils.schemaName
+import APIs.DoctorAPI.SeatMessage
 
-case class TeacherAddSeatPlanner(seatNumber: String, override val planContext: PlanContext) extends Planner[String] {
+case class TeacherAddSeatPlanner(message: SeatMessage, override val planContext: PlanContext) extends Planner[String] {
   override def plan(using PlanContext): IO[String] = {    /** 插入座位信息到数据库 */
     writeDB(
       s"INSERT INTO $schemaName.seats (seat_number, status) VALUES (?, ?)",
-      List(SqlParameter("String", seatNumber), SqlParameter("String", "available"))
+      List(SqlParameter("String", message.SeatNumber), SqlParameter("String", "available"))
     ).map(_ => "Seat added successfully")
   }
 }
--- doctor/src/main/scala/Impl/TeacherDeleteSeatPlanner.scala
+++ doctor/src/main/scala/Impl/TeacherDeleteSeatPlanner.scala
@@ -6,12 +6,13 @@ import Common.API.{PlanContext, Planner}
 import Common.DBAPI.writeDB
 import Common.Object.SqlParameter
 import Common.ServiceUtils.schemaName
+import  APIs.DoctorAPI.SeatMessage
 
-case class TeacherDeleteSeatPlanner(seatNumber: String, override val planContext: PlanContext) extends Planner[String] {
+case class TeacherDeleteSeatPlanner(message: SeatMessage, override val planContext: PlanContext) extends Planner[String] {
   override def plan(using PlanContext): IO[String] = {
     writeDB(
       s"DELETE FROM $schemaName.seats WHERE id = ?",
-      List(SqlParameter("String", seatNumber), SqlParameter("String", "available"))
+      List(SqlParameter("String", message.SeatNumber), SqlParameter("String", "available"))
     ).map(_ => "Seat deleted successfully")
   }
 }
